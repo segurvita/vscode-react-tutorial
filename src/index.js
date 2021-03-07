7503d3dffa78fc8b5cc827b5cc565712102e9ba8
segur <segur.opus@gmail.com>
2021-03-07 22:24:36 +0900

    Remove App from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
